60f0bcf443055311684ec71c55365693bb94bc85
MetallicSquid <guillame.Macneil@ccoex.com>
2020-04-24 15:25:21 +0100

    Commit for build_model.py and evaluate_accuracy.py

--- digit-classifier/build_model.py
+++ digit-classifier/build_model.py
@@ -1,6 +1,38 @@
+# The digit recognition model
+
 # Necessary imports
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
 from keras.datasets import mnist
+def build_model(epochs=10):
+    # Gather the dataset and split it into train and test sets, normalize this data between 0 and 1
+    (image_train, label_train), (image_test, label_test) = mnist.load_data()
+    image_train = image_train / 255.0
+    image_test = image_test / 255.0
 
-# Gather the dataset and split it into train and test sets
-(image_train, label_train), (image_test, label_test) = mnist.load_data()
+    # Build the model
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(10)
+    ])
 
+    model.compile(optimizer='adam',
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'])
+
+    # Train the model on the train dataset and print the accuracy and loss
+    model.fit(image_train, label_train, epochs=10)
+    test_loss, test_acc = model.evaluate(image_test, label_test, verbose=2)
+
+    print('\nTest accuracy: ', test_acc, '\nTest loss:', test_loss)
+
+    # Make a predictor model
+    prob_model = tf.keras.Sequential([model,
+        tf.keras.layers.Softmax()])
+
+    # Return the model as the output of the `build_model` function
+    return [image_test, label_test, prob_model]
---
+++ digit-classifier/evaluate_accuracy.py
@@ -0,0 +1,54 @@
+# Import the `build_model` function from `build_model.py`
+from build_model import build_model
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the built model
+image_test, label_test, prob_model = build_model()
+
+predictions = prob_model.predict(image_test)
+
+# A list of class names
+class_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+# Plot the predictions (taken from tensorflow classification tutorial)
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        colour = 'blue'
+    else:
+        colour = 'red'
+
+    plt.xlabel("{} {:2.0f} ({})".format(class_names[predicted_label],
+        100*np.max(predictions_array),
+        class_names[true_label]),
+        color=colour)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color='#777777')
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+# NB: Press 'q' to move to the next digit in the prediction
+for i in range(25):
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i, predictions[i], label_test, image_test)
+    plt.subplot(1, 2, 2)
+    plot_value_array(i, predictions[i], label_test)
+    plt.show()
