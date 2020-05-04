7c6a432eb96780ab638e8536e58b097efb10f201
MetallicSquid <guillame.Macneil@ccoex.com>
2020-05-04 15:17:32 +0100

    Commit for maze_creator.py

---
+++ maze-navigator/maze_creator.py
@@ -0,0 +1,159 @@
+# A script that makes a maze to the user's specification.
+# This is heavily influenced by `https://scipython.com/blog/making-a-maze/`
+
+# Necessary imports
+import random
+
+# A cell in the maze
+class Cell:
+    # Separate a pair of cells in N-S or W-E directions
+    wall_pairs = {'N': 'S', 'S': 'N', 'E': 'W', 'W': 'E'}
+
+    # Initialize the cell at x, y - at first it is surrounded by walls
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.walls = {'N': True, 'S': True, 'E': True, 'W': True}
+
+    # Does the cell have all of its walls
+    def has_all_walls(self):
+        return all(self.walls.values())
+
+    # Knock down the wall between current cell and other
+    def knock_down_wall(self, other, wall):
+        self.walls[wall] = False
+        other.walls[Cell.wall_pairs[wall]] = False
+
+# A maze represented as a grid of cells
+class Maze:
+    # Initialize the maze grid
+    def __init__(self, nx, ny, ix=0, iy=0):
+        self.nx, self.ny = nx, ny
+        self.ix, self.iy = ix, iy
+        self.maze_map = [[Cell(x, y) for y in range(ny)] for x in range(nx)]
+
+    # Return the Cell at x, y
+    def cell_at(self, x, y):
+        return self.maze_map[x][y]
+
+    # Return a string representation of the maze
+    def __str__(self):
+        maze_rows = ['-' * nx*2]
+        for y in range(ny):
+            maze_row = ['|']
+            for x in range(nx):
+                if self.maze_map[x][y].walls['E']:
+                    maze_row.append(' |')
+                else:
+                    maze_row.append('  ')
+            maze_rows.append(''.join(maze_row))
+            maze_row = ['|']
+            for x in range(nx):
+                if self.maze_map[x][y].walls['S']:
+                    maze_row.append('-+')
+                else:
+                    maze_row.append(' +')
+            maze_rows.append(''.join(maze_row))
+        return '\n'.join(maze_rows)
+
+    # Write an SVG image of the maze
+    def write_svg(self, filename):
+        aspect_ratio = self.nx / self.ny
+
+        # Pad the maze by this amount
+        padding = 10
+
+        # Height and width of the maze, excluding padding
+        height = 500
+        width = int(height * aspect_ratio)
+
+        # Scaling factors mapping maze coordinates to image coordinates
+        scy, scx = height / ny, width / nx
+
+        # Write a single wall to the SVG image file handle f
+        def write_wall(f, x1, y1, x2, y2):
+            print('<line x1="{}" y1="{}" x2="{}" y2="{}"/>'.format(x1, y1, x2, y2), file=f)
+
+        # Write the SVG image file for maze
+        with open(filename, 'w') as f:
+            # SVG preamble and styles.
+            print('<?xml version="1.0" encoding="utf-8"?>', file=f)
+            print('<svg xmlns="http://www.w3.org/2000/svg"', file=f)
+            print('    xmlns:xlink="http://www.w3.org/1999/xlink"', file=f)
+            print('    width="{:d}" height="{:d}" viewBox="{} {} {} {}">'
+                    .format(width+2*padding, height+2*padding,
+                        -padding, -padding, width+2*padding, height+2*padding),
+                  file=f)
+            print('<defs>\n<style type="text/css"><![CDATA[', file=f)
+            print('line {', file=f)
+            print('    stroke: #000000;\n    stroke-linecap: square;', file=f)
+            print('    stroke-width: 5;\n}', file=f)
+            print(']]></style>\n</defs>', file=f)
+            # Draw the "South" and "East" walls of each cell, if present (these
+            # are the "North" and "West" walls of a neighbouring cell in
+            # general, of course).
+            for x in range(nx):
+                for y in range(ny):
+                    if maze.cell_at(x,y).walls['S']:
+                        x1, y1, x2, y2 = x*scx, (y+1)*scy, (x+1)*scx, (y+1)*scy
+                        write_wall(f, x1, y1, x2, y2)
+                    if maze.cell_at(x,y).walls['E']:
+                        x1, y1, x2, y2 = (x+1)*scx, y*scy, (x+1)*scx, (y+1)*scy
+                        write_wall(f, x1, y1, x2, y2)
+            # Draw the North and West maze border, which won't have been drawn
+            # by the procedure above.
+            print('<line x1="0" y1="0" x2="{}" y2="0"/>'.format(width), file=f)
+            print('<line x1="0" y1="0" x2="0" y2="{}"/>'.format(height),file=f)
+            print('</svg>', file=f)
+
+    # Return a list of unvisited neighbours to cell.
+    def find_valid_neighbours(self, cell):
+
+        delta = [('W', (-1,0)),
+                 ('E', (1,0)),
+                 ('S', (0,1)),
+                 ('N', (0,-1))]
+        neighbours = []
+        for direction, (dx,dy) in delta:
+            x2, y2 = cell.x + dx, cell.y + dy
+            if (0 <= x2 < nx) and (0 <= y2 < ny):
+                neighbour = maze.cell_at(x2, y2)
+                if neighbour.has_all_walls():
+                    neighbours.append((direction, neighbour))
+        return neighbours
+
+    def make_maze(self):
+        # Total number of cells.
+        n = self.nx * self.ny
+        cell_stack = []
+        current_cell = self.cell_at(ix, iy)
+        # Total number of visited cells during maze construction.
+        nv = 1
+
+        while nv < n:
+            neighbours = self.find_valid_neighbours(current_cell)
+
+            if not neighbours:
+                # We've reached a dead end: backtrack.
+                current_cell = cell_stack.pop()
+                continue
+
+            # Choose a random neighbouring cell and move to it.
+            direction, next_cell = random.choice(neighbours)
+            current_cell.knock_down_wall(next_cell, direction)
+            cell_stack.append(current_cell)
+            current_cell = next_cell
+            nv += 1
+
+# Maze dimensions (ncols, nrows)
+nx, ny = 20, 20
+# Maze entry position
+ix, iy = 0, 0
+
+maze = Maze(nx, ny, ix, iy)
+maze.make_maze()
+
+print(maze)
+maze.write_svg('maze.svg')
+
+
+
